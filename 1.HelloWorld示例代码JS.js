1d51d99bb76e92537bd43aadde50d1bcb145acde
oca-john <40886062+oca-john@users.noreply.github.com>
2019-10-22 18:40:20 +0800

    Create 1.HelloWorld示例代码JS.js

---
+++ 1.HelloWorld示例代码JS.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Hello World!</title>
+    <script type="text/javascript">
+      document.write("Hello World!");    // 在网页中显示。
+      alert("Hello World!");             // 以对话框的形式显示。
+    </script>
+  </head>
+  <body>
+    <p>网页内容</p>
+  </body>
+</html>
