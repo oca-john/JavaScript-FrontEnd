31e55882e9e1ed269d77466e74e4df38092f9c7e
oca-john <40886062+oca-john@users.noreply.github.com>
2021-01-16 09:59:45 +0800

    Update 1.HelloWorld示例代码JS.js

--- 1.HelloWorld示例代码JS.js
+++ 1.HelloWorld示例代码JS.js
@@ -3,11 +3,12 @@
   <head>
     <title>Hello World!</title>
     <script type="text/javascript">      // JS 本质上就是嵌入到网页文件中的 <script> 代码块，用于指定网页的交互方式
-      document.write("Hello World!");    // 在网页中显示。
-      alert("Hello World!");             // 以对话框的形式显示。
+                                         // <script>块中默认就是JS类型，可以不声明类型
+      document.write("Hello World!");    // 在网页中写入文本。
+      alert("Hello World!");             // 以对话框的形式显示（也可以直接显示）。
     </script>
   </head>
   <body>
-    <p>网页内容</p>
+    <p>网页内容</p>                       // html中其他内容
   </body>
 </html>
