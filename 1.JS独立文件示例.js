4c0e5017e6c150225c66168fd4e21eda4ce38658
oca-john <40886062+oca-john@users.noreply.github.com>
2021-01-23 10:52:37 +0800

    Update 1.JS独立文件示例.js

--- 1.JS独立文件示例.js
+++ 1.JS独立文件示例.js
@@ -1,3 +1,6 @@
 function ShowMess(){                        // 独立JS文件只包含html文件中<script>部分代码
 alert("Hello JS, Input your name please.");
 }
+
+// Chrome浏览器中调试F12，选Console，可直接以交互模式使用JS代码
+// `window.alert('Hello JavaScript!')`, 会直接显示弹出式对话框显示信息
