d1dd6702e6679cf3116f3594fe292512c0565757
oca-john <40886062+oca-john@users.noreply.github.com>
2021-05-06 20:13:43 +0800

    Update 4.PHP.Learn.基本语法.php

--- 4.PHP.Learn.基本语法.php
+++ 4.PHP.Learn.基本语法.php
@@ -22,6 +22,50 @@
   
   $x = "Hello world!";
   $x = 'Hello world!';        // 单双引号均可引住字串
+  
+  // if 语句
+  $t=date("H");               // php 内的 if 语句
+  if ($t<"20")
+  {
+      echo "Have a good day!";
+  }
+  
+  // switch 条件语句
+  switch (n)                  // php内的 switch 语句
+  {
+  case label1:
+      如果 n=label1，此处代码将执行;
+      break;
+  case label2:
+      如果 n=label2，此处代码将执行;
+      break;
+  default:
+      如果 n 既不等于 label1 也不等于 label2，此处代码将执行;
+  }
+  
+  // 数组
+  $cars=array("Volvo","BMW","Toyota");    // 创建并打印数组
+  echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+  
+  // 函数
+  function writeName()        // 定义函数
+  {
+      echo "Kai Jim Refsnes"; // 函数内部的代码块
+  }
+  echo "My name is ";         // 打印字符串及变量（通过调用函数）
+  writeName();
+  
+  // 数据库
+  $servername = "localhost";  // 连接 mysql 数据库，填写数据库和用户信息
+  $username = "username";
+  $password = "password";
+  // 创建连接
+  $conn = new mysqli($servername, $username, $password);
+  // 检测连接
+  if ($conn->connect_error) {
+      die("连接失败: " . $conn->connect_error);
+  }
+  echo "连接成功";
 ?>  
 
 </body>  
