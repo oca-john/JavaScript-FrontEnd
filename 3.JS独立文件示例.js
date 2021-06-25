0b84f2f0e61393365d90a767a0435e662148a98b
oca-john <40886062+oca-john@users.noreply.github.com>
2021-06-26 01:03:59 +0800

    Update 3.JS独立文件示例.js

--- 3.JS独立文件示例.js
+++ 3.JS独立文件示例.js
@@ -4,3 +4,5 @@ alert("Hello JS, Input your name please."); // 注意不包含<script>标记
 
 // Chrome浏览器中调试F12，选Console，可直接以交互模式使用JS代码
 // `window.alert('Hello JavaScript!')`, 会直接显示弹出式对话框显示信息
+
+// HTML构建基本界面元素，CSS实现布局优化，JS增加页面动作，则可以在网页上实现所有视觉内容。如果基于React货VUE这样的本地JS框架，则可以用于构建具备交互能力的本地应用程序。
